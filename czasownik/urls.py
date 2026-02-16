389ee946c1f2ec4a6c883679a57227ef648a9941
PatrykPod <pat93p@gmail.com>
2026-02-16 01:16:18 +0100

    clean up pyc files + add frontend

--- czasownik/urls.py
+++ czasownik/urls.py
@@ -1,7 +1,17 @@
+from django.http import FileResponse
+import os
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+
+
+def frontend(request):
+    file_path = os.path.join(settings.BASE_DIR, 'static/index.html')
+    return FileResponse(open(file_path, 'rb'))
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('logs.urls')),
+    path('', frontend),
 ]
